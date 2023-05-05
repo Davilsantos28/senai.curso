2346eb75e0eba3e04d84526ba8d4eac0c2436f99
Davilsantos28 <davilsantos333@gmail.com>
2023-05-04 21:23:34 -0300

    index.html

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//efeito do botão voltar ao Topo
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value; 
+    var senha = document.getElementById("senha").value;
+    
+    if(usuario == 'admin' && senha == '123456'){ 
+      window.location="index.html";
+    logado = 1;
+}
+    if(logado == 0){
+    
+    alert("Acesso Negado. Dados incorretos");
+    }
+    }
+
+//Ativar alert no botão cadastrar
+function cadastro(){
+    alert("Cadastrado com sucesso!")
+    window.location.href = "index.html";
+}
